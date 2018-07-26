c6cff32e293ab3644c1e31246f1929fea5fdecf3
Shaniqua <asumahshaniqua@gmail.com>
2018-07-26 07:01:56 -0700

    first commit

---
+++ layout_header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>
+      <!-- Latest compiled and minified Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+
+    <!-- our custom CSS -->
+    <link rel="stylesheet" href="libs/css/custom.css" />
+
+
+    </title>
+  </head>
+  <body>
+    <div class="container">
+
+            <?php
+            // show page header
+            echo "<div class='page-header'>
+                    <h1>{$page_title}</h1> The page title!!
+                </div>";
+            ?>
