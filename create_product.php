299c8cbe07073cf4555852034aa8e989997da703
Shaniqua <asumahshaniqua@gmail.com>
2018-07-29 08:50:55 -0700

    saving changes

---
+++ create_product.php
@@ -0,0 +1,23 @@
+<?php
+$page_title = "Create Product";
+include_once "layout_header.php";
+
+
+echo "<div class='right-button-margin'>";
+    echo "<a href='index.php' class='btn btn-default pull-right'>Read Products</a>";
+echo "</div>";
+
+?>
+<!-- 'create product' html form will be here -->
+<?php
+
+
+
+
+
+
+
+
+
+include_once "layout_footer.php";
+?>
--- layout_header.php
+++ layout_header.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>
-      <!-- Latest compiled and minified Bootstrap CSS -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title> <?php echo $page_tile; ?> </title>
+      <!-- Latest compiled and minified Bootstrap CSS-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 
     <!-- our custom CSS -->
-    <link rel="stylesheet" href="libs/css/custom.css" />
-
-
-    </title>
+    <link rel="stylesheet" href="css/style.css" />
   </head>
   <body>
     <div class="container">
@@ -18,6 +18,6 @@
             <?php
             // show page header
             echo "<div class='page-header'>
-                    <h1>{$page_title}</h1> The page title!!
+                    <h1>{$page_title}</h1>
                 </div>";
             ?>
